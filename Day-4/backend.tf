4d9d8836bee3e66c89f1f53dada3a753157547c8
Shreyushi T R <Shreyushi@gmail.com>
2026-04-03 13:06:22 +0000

    practice of day-4

--- Day-4/backend.tf
+++ Day-4/backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket         = "abhishek-s3-demo-xyz" # change this
-    key            = "abhi/terraform.tfstate"
-    region         = "us-east-1"
+    bucket         = "shreyushi-s3-demo-xyz" # change this
+    key            = "shreya/terraform.tfstate"
+    region         = "eu-north-1"
     encrypt        = true
     dynamodb_table = "terraform-lock"
   }
--- Day-4/main.tf
+++ Day-4/main.tf
@@ -1,15 +1,14 @@
 provider "aws" {
-  region = "us-east-1"
+  region = "eu-north-1"
 }
 
-resource "aws_instance" "abhishek" {
-  instance_type = "t2.micro"
-  ami = "ami-053b0d53c279acc90" # change this
-  subnet_id = "subnet-019ea91ed9b5252e7" # change this
+resource "aws_instance" "shreyushi" {
+  instance_type = "t3.micro"
+  ami = "ami-0aaa636894689fa47" # change this
 }
 
 resource "aws_s3_bucket" "s3_bucket" {
-  bucket = "abhishek-s3-demo-xyz" # change this
+  bucket = "shreyushi-s3-demo-xyz" # change this
 }
 
 resource "aws_dynamodb_table" "terraform_lock" {
